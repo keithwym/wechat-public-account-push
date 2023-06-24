20d0e8b8e436790765ec790284eede02768bbbac
wym keith <88230256+keithwym@users.noreply.github.com>
2023-06-24 09:54:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,11 +49,11 @@ USE_PASSAGE: 'push-deer'
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'Keith',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZAfc6RnG_-1zrlQnLCa5XEg8fgg',
     }
